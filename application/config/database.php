a3e92799ae7fcb11374067fe4ec01a2ef6e21556
azkajihan09 <azkajihan09@gmail.com>
2025-11-27 14:26:28 +0800

    kadatahu123db

--- application/config/database.php
+++ application/config/database.php
@@ -5,35 +5,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-// $db['default'] = array(
-// 	'dsn'	=> '',
-// 	'hostname' => 'localhost',
-// 	'username' => 'root',
-// 	'password' => 'kadatahu123db',
-// 	'database' => 'sipp',
-// 	'dbdriver' => 'mysqli',
-// 	'dbprefix' => '',
-// 	'pconnect' => FALSE,
-// 	'db_debug' => (ENVIRONMENT !== 'production'),
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => TRUE
-// );
-
-
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => '',
-	'database' => 'sipp_terbaru5',
+	'password' => 'kadatahu123db',
+	'database' => 'sipp',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -50,13 +27,13 @@ $db['default'] = array(
 	'save_queries' => TRUE
 );
 
-// // Database kedua untuk sistem pencatatan reminder BHT
-// $db['reminder_db'] = array(
+
+// $db['default'] = array(
 // 	'dsn'	=> '',
 // 	'hostname' => 'localhost',
 // 	'username' => 'root',
 // 	'password' => '',
-// 	'database' => 'catatan_bht',
+// 	'database' => 'sipp_terbaru5',
 // 	'dbdriver' => 'mysqli',
 // 	'dbprefix' => '',
 // 	'pconnect' => FALSE,
@@ -73,12 +50,13 @@ $db['default'] = array(
 // 	'save_queries' => TRUE
 // );
 
-// Database ketiga untuk sistem notelen
+
+// Database kedua untuk sistem notelen
 $db['notelen_db'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => '',
+	'password' => 'kadatahu123db',
 	'database' => 'catatan_bht',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
